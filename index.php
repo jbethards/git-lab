a5e3855d7f95a421d9326939b846509e6644dd6c
Alex Krytskyi <okrytskyi2@mail.greenriver.edu>
2018-10-30 22:36:34 -0400

    Added another route

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ $f3->route('GET /', function (){
           <p>Hello from Joseph</p>'
     ;
 });
+//Define a default route
+$f3->route('GET /page1', function (){
+    echo '<h1>Git Lab: Page 1 is done.</h1>'
+    ;
+});
 
 // Run fat free
 $f3->run();
